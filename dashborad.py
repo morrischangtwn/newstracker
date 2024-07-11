21b6b25fe68bf24c1a444d3dc4f6e162861623ac
morrischangtwn <morrischangtwn@gmail.com>
2024-07-12 01:01:53 +0800

    Update dashborad.py

--- dashborad.py
+++ dashborad.py
@@ -57,6 +57,8 @@ start_date = st.sidebar.date_input("Start Date", current_date - timedelta(days=1
 end_date = st.sidebar.date_input("End Date", current_date)
 date_list = pd.date_range(start=start_date, end=end_date, freq='D').date.tolist()
 
+date_list = [date.strftime('%Y-%m-%d') for date in date_list]
+
 user_input_text = st.sidebar.text_input("Enter keyword:",value='Taiwan',max_chars=15,)
 
 def remove_items(row):
@@ -67,6 +69,7 @@ def get_news_in_time(date_list, user_input_text):
     result_df = pd.DataFrame()
     for i in range(len(date_list)-1):
         print('day', date_list[i],date_list[i+1])
+        print('day', type(date_list[i]), type(date_list[i+1]))
         googlenews = GoogleNews(start=date_list[i],end= date_list[i+1])
         googlenews.get_news(user_input_text)
         temp_df = pd.DataFrame((googlenews.results()))
